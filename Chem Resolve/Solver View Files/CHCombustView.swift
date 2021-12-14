df8667b1361bdc9032b19402599786eeefb11439
ThoGos <tgosport@gmail.com>
2021-12-13 23:20:38 -0800

    CxHy Combustion Analysis first in the world is not out

--- Chem Resolve/Solver View Files/CHCombustView.swift
+++ Chem Resolve/Solver View Files/CHCombustView.swift
@@ -67,7 +67,7 @@ struct CHCombustView: View {
                     Text("Molecular Formula of Hydrocarbon:")
                         .fontWeight(.semibold)
                     
-                    Text("bruh")
+                    Text(getMolFor(wat: h2o, carb: co2, wfor: molmass))
                 }
                 
                 
@@ -84,28 +84,193 @@ struct CHCombustView: View {
 
 func getEmpire(wat: String, carb: String) -> String {
     
-    var mh2o = ((Float(wat) ?? 0.0)/18) * 2.0
-    var mco2 = ((Float(carb) ?? 0.0)/44)
-    var xy = Double(mco2/mh2o)
-    var yx = Double(mh2o/mh2o)
+    let hmol = ((Double(wat) ?? 0.0)/18) * 2
+    let cmol = ((Double(carb) ?? 0.0)/44)
+    var xbig = 0.0
+    var ysmall = 0.0
+    var factor = 0.0
+    var intx = 0
+    var inty = 0
+    var horc = false
+    
     
     if(wat == "" || carb == "") {
         return "None"
     }
     
-    if(floor(xy) != xy || floor(yx) != yx) {
-        xy = xy*2
-        yx = yx*2
+    if(cmol > hmol && hmol != 0) {
+        xbig = cmol/hmol
+        ysmall = 1.0
+        horc = true
     }
     
-    xy = xy.rounded(.towardZero)
+    if(hmol > cmol && cmol != 0) {
+        xbig = hmol/cmol
+        ysmall = 1.0
+        horc = false
+    }
     
-    yx = yx.rounded(.towardZero)
     
-    return "C" + String(xy) + "H" + String(yx)
+    if(floor(xbig) != xbig || floor(ysmall) != ysmall) {
+        
+        xbig = round(xbig*100) / 100.0
+        ysmall = round(ysmall*100) / 100.0
+        
+    }
+    
+    
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 100)) == 25){
+        factor = 4.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 100)) == 33){
+        factor = 3.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 100)) == 66){
+        factor = 3.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 2){
+        factor = 5.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 5){
+        factor = 2.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 4){
+        factor = 5.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 8){
+        factor = 5.0
+    }
+    
+    
+    
+    if(floor(xbig) != xbig || floor(ysmall) != ysmall) {
+        xbig = xbig*factor
+        ysmall = ysmall*factor
+    }
+
+    
+    intx = Int(xbig.rounded(.towardZero))
+    
+    inty = Int(ysmall.rounded(.towardZero))
+    
+    if(horc) {
+        return "C" + String(intx) + "H" + String(inty)
+    }
+    else {
+        return "C" + String(inty) + "H" + String(intx)
+    }
+    
 }
 
 
+func getMolFor(wat: String, carb: String, wfor: String) -> String {
+    
+    let hmol = ((Double(wat) ?? 0.0)/18) * 2
+    let cmol = ((Double(carb) ?? 0.0)/44)
+    var xbig = 0.0
+    var ysmall = 0.0
+    var factor = 0.0
+    var factor2 = 1
+    var intx = 0
+    var inty = 0
+    var horc = false
+    let howmuch = Int(wfor) ?? 1
+    
+    
+    if(wfor == "") {
+        return "None"
+    }
+    
+    if(cmol > hmol && hmol != 0) {
+        xbig = cmol/hmol
+        ysmall = 1.0
+        horc = true
+    }
+    
+    if(hmol > cmol && cmol != 0) {
+        xbig = hmol/cmol
+        ysmall = 1.0
+        horc = false
+    }
+    
+    
+    if(floor(xbig) != xbig || floor(ysmall) != ysmall) {
+        
+        xbig = round(xbig*100) / 100.0
+        ysmall = round(ysmall*100) / 100.0
+        
+    }
+    
+    
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 100)) == 25){
+        factor = 4.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 100)) == 33){
+        factor = 3.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 100)) == 66){
+        factor = 3.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 2){
+        factor = 5.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 5){
+        factor = 2.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 4){
+        factor = 5.0
+    }
+    
+    if((Int(Double(xbig) * 10) - (Int(xbig) * 10)) == 8){
+        factor = 5.0
+    }
+    
+    
+    
+    if(floor(xbig) != xbig || floor(ysmall) != ysmall) {
+        xbig = xbig*factor
+        ysmall = ysmall*factor
+    }
+
+    
+    intx = Int(xbig.rounded(.towardZero))
+    
+    inty = Int(ysmall.rounded(.towardZero))
+    
+    
+    if(horc && intx != 0) {
+        factor2 = howmuch/(intx*12 + inty)
+    }
+    else if(!horc && intx != 0) {
+        factor2 = howmuch/(inty*12 + intx)
+    }
+
+    
+    intx = intx*factor2
+    
+    inty = inty*factor2
+    
+    if(horc) {
+        return "C" + String(intx) + "H" + String(inty)
+    }
+    else {
+        return "C" + String(inty) + "H" + String(intx)
+    }
+    
+}
 
 
 
